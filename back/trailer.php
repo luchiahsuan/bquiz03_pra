d0e8e8e92ff333b93ec3fed6f8f8cb19a850f035
chiahsuanlu <--lu2010_81509@yahoo.com.tw>
2023-01-30 15:38:14 +0800

    使用ajax的方式完成預告片排序變更

--- back/trailer.php
+++ back/trailer.php
@@ -11,7 +11,8 @@
             <?php
             $ts = $Trailer->all(" ORDER BY `rank`");
             foreach ($ts as $key => $t) {
-
+                $prev = ($key == 0) ? $t['id'] : $ts[$key - 1]['id'];
+                $next = ($key == (count($ts) - 1)) ? $t['id'] : $ts[$key + 1]['id'];
             ?>
                 <div style="display: flex;align-items:center;justify-content:center;text-align:center">
 
@@ -22,16 +23,16 @@
                         <input type="text" name="name[]" value="<?= $t['name']; ?>">
                     </div>
                     <div style="width:25%">
-                        <input type="button" value="往上">
-                        <input type="button" value="往下">
+                        <input type="button" value="往上" onclick="sw(<?= $t['id']; ?>,<?= $prev; ?>)">
+                        <input type="button" value="往下" onclick="sw(<?= $t['id']; ?>,<?= $next; ?>)">
                     </div>
                     <div style="width:25%">
                         <input type="checkbox" name="sh[]" value="<?= $t['id']; ?>" <?= ($t['sh'] == 1) ? 'checked' : '' ?>>顯示&nbsp;
                         <input type="checkbox" name="del[]" value="<?= $t['id']; ?>">刪除&nbsp;
                         <select name="ani[]">
-                            <option value="1" <?=($t['ani']==1)?'selected':'' ;?>>淡入淡出</option>
-                            <option value="2" <?=($t['ani']==2)?'selected':'' ;?>>滑入滑出</option>
-                            <option value="3" <?=($t['ani']==3)?'selected':'' ;?>>縮放</option>
+                            <option value="1" <?= ($t['ani'] == 1) ? 'selected' : ''; ?>>淡入淡出</option>
+                            <option value="2" <?= ($t['ani'] == 2) ? 'selected' : ''; ?>>滑入滑出</option>
+                            <option value="3" <?= ($t['ani'] == 3) ? 'selected' : ''; ?>>縮放</option>
                         </select>
                         <input type="hidden" name="id[]" value="<?= $t['id']; ?>">
                     </div>
@@ -63,3 +64,12 @@
         <input type="reset" value="重置">
     </div>
 </form>
+
+<script>
+function sw(id1,id2){
+    $.post("./api/sw.php",{id1,id2},()=>{
+        location.reload();
+    })
+}
+
+</script>
