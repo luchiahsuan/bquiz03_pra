69f826500303b18b6fed70060508e5d25a8827cc
chiahsuanlu <--lu2010_81509@yahoo.com.tw>
2023-02-13 13:21:03 +0800

    完成刪除單筆訂單資料功能

--- js/js.js
+++ js/js.js
@@ -4,9 +4,18 @@ function sw(table,id1,id2){
     })
 }
 
-function del(table,id){
+function del(table,id,msg){
 
-    let chk=confirm("是否確定要刪除");
+
+    let chk;
+    if(typeof(msg)!='unfefind'){
+    chk=confirm(msg);
+
+    }else{
+
+        chk=confirm("是否確定要刪除");
+    }
+    
     if(chk){
         $.post('./api/del.php',{table,id},()=>{
             location.reload();
