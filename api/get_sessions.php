18f99865ad41cd900932af46625115e895366522
chiahsuanlu <--lu2010_81509@yahoo.com.tw>
2023-02-13 11:27:19 +0800

    完成訂票選單中的剩餘座位計算

--- api/get_sessions.php
+++ api/get_sessions.php
@@ -1,20 +1,30 @@
 <?php
 include_once "base.php";
 
-$movie=$Movie->find($_GET['id']);
-$date=$_GET['date'];
+$movie = $Movie->find($_GET['id']);
+$date = $_GET['date'];
 
-$hr=date("G");
+$hr = date("G");
 
-if($date==date("Y-m-d") && $hr>=14){
-    $start=floor($hr/2)-5;
-}else{
-    $start=1;
+if ($date == date("Y-m-d") && $hr >= 14) {
+    $start = floor($hr / 2) - 5;
+} else {
+    $start = 1;
 };
-for($i=$start;$i<5;$i++){
-echo "<option value='{$Movie->session[$i]}'>";
-echo $Movie->session[$i];
-echo "剩餘座位 20";
-echo "</option>";
-}
+for ($i = $start; $i < 5; $i++) {
 
+    // $orders = $Order->all(['movie' => $movie['name'], 'date' => $date, 'session' => $Movie->session[$i]]);
+    // $sum = 0;
+    // foreach ($orders as $order) {
+    //     $seats=unserialize($order['seats']);
+    //     $num = count($seats);
+    //     $sum += $num;
+    // }
+
+    $sum = $Order->sum('qt', ['movie' => $movie['name'], 'date' => $date, 'session' => $Movie->session[$i]]);
+
+    echo "<option value='{$Movie->session[$i]}'>";
+    echo $Movie->session[$i];
+    echo "剩餘座位" . (20 - $sum);
+    echo "</option>";
+}
